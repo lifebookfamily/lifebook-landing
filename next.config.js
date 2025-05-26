d34c0a9c8829dd50b30bd68f5cf7893da82c6cda
Manupri974 <emmanuel.prianon@gmail.com>
2025-05-25 20:28:28 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true, // 👈 active le système basé sur /app
+  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
